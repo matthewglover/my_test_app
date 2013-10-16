263c1fbe95a723f03bb719bdaee0342de5e9960b
Matt Glover <matthew.glover@peeq-digital.com>
2013-10-16 16:11:28 +0100

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,10 +57,19 @@ describe "UserPages" do
   ############### Profile page #####################
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
 
@@ -115,6 +124,7 @@ describe "UserPages" do
     end
   end
 
+  ############### Edit page #####################
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
